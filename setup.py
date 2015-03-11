b1f689f82bbb6d26511b6a310be798dad1791fc5
Wouter van Atteveldt <wouter@vanatteveldt.com>
2015-03-11 21:51:38 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+version='0.10',
+name="saf",
+description="Python toolkit for handling Simple Annotation Framework files",
+author="Wouter van Atteveldt",
+author_email="wouter@vanatteveldt.com",
+packages=["saf"],
+classifiers=[
+"License :: OSI Approved :: MIT License",
+],
+)
